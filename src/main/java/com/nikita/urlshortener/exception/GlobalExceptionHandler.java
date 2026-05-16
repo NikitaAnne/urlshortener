894ab923f32305c083ba36bb0298518ef8a4f6e1
NikitaAnne <83005307+NikitaAnne@users.noreply.github.com>
2026-05-16 18:03:41 +0200

    Implemented global exception handling

---
+++ src/main/java/com/nikita/urlshortener/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.nikita.urlshortener.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            ResourceNotFoundException ex) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(
+                errorResponse,
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
